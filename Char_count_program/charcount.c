c38ddce7cfd48aafbdf1efb3fe55fa014b495b70
McCoy <mccoybecker@gmail.com>
2018-07-28 10:18:00 -0700

    Char count program

---
+++ Char_count_program/charcount.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* A program for counting chars in input */
+
+int main()
+{
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc){
+    ;
+    }
+    printf("%.0f\n",nc);
+}
